ee662d9b5c219aa404f465f7e4eed999faf34fb0
Riccardo Salemme <riccardo.salemme.2005@gmail.com>
2019-03-14 21:36:08 +0100

    Revisione

--- boards/esp8266-01/src/main.cpp
+++ boards/esp8266-01/src/main.cpp
@@ -1,5 +1,7 @@
 //esp8266 libraries
 #include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
 #include <WiFiClient.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WebServer.h>
@@ -26,7 +28,7 @@ const char *password = "abcabcabc";
 //my includes
 #include "index_html.h " //html page
 
-
+ESP8266WiFiMulti wifiMulti;
 ESP8266WebServer server(80);
 SCD30 airSensor;
 
@@ -36,14 +38,23 @@ SCD30 airSensor;
 void setup() {
   Wire.begin(0,2);
   EEPROM.begin(512);
+  Serial.setDebugOutput(true);
   delay(1000);
   Serial.begin(115200);
   Serial.println();
   Serial.println("Provo a connettermi al WIFI (credenziali salvate nella EEPROM)");
 
-  WiFi.persistent(false);
+  //WiFi.persistent(false);
 
-  _start();
+  //_start();
+
+  if (connect_wifi() == 1) {
+    Serial.println("Connesso!!!!!");
+  }else{
+    Serial.println("No connesso!!!!!!");
+  }
+
+
 
   airSensor.begin(); //This will cause readings to occur every two seconds
   delay(5000);
@@ -52,28 +63,29 @@ void setup() {
 
 void loop() {
 
-  server.handleClient();
 
-  int currentMillis = millis();
-  yield();
-
-  if (currentMillis > lastSampleTime){
-
-    if(WiFi.status()== WL_CONNECTED){
-      if(!scd30_read() == 1){
-        Serial.println("ERROR: Sensor data is unattainable or delay time is too small (min. delay for Thingspeak is 15 sec.)");
-        sensor_status = 0;
-      }else{
-        sensor_status = 1;
-      }
-    }else{
-        Serial.println("Error in WiFi connection");
-        delay(2000);
-    }
-    //Serial.println(lastSampleTime);
-    lastSampleTime = currentMillis + read_String(1).toInt()*1000;
-    //Serial.println(lastSampleTime);
-    Serial.println("____________________________");
-    yield();
-  }
+  // server.handleClient();
+  //
+  // int currentMillis = millis();
+  // yield();
+  //
+  // if (currentMillis > lastSampleTime){
+  //
+  //   if(WiFi.status()== WL_CONNECTED){
+  //     if(!scd30_read() == 1){
+  //       Serial.println("ERROR: Sensor data is unattainable or delay time is too small (min. delay for Thingspeak is 15 sec.)");
+  //       sensor_status = 0;
+  //     }else{
+  //       sensor_status = 1;
+  //     }
+  //   }else{
+  //       Serial.println("Error in WiFi connection");
+  //       delay(2000);
+  //   }
+  //   //Serial.println(lastSampleTime);
+  //   lastSampleTime = currentMillis + read_String(1).toInt()*1000;
+  //   //Serial.println(lastSampleTime);
+  //   Serial.println("____________________________");
+  //   yield();
+  // }
 }
